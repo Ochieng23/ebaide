c59e81939443cf2b56b24a2c1190d852695b2992
paullaster <paullasterokoth98@gmail.com>
2022-12-24 15:19:13 +0300

    adding changes to project files

--- src/register.js
+++ src/register.js
@@ -7,7 +7,18 @@ const ElementById = (id) =>{
 ElementById('reg_form').addEventListener ('submit', getData);
 
 const getData = () => {
-   
+   const firstname = ElementById('firstname').value;
+   const lastname = ElementById('lastname').value;
+   const phonenumber = ElementById('phonenumber').value;
+   const email = ElementById('email').value;
+   const password= ElementById('password').value;
+   console.log ({
+    firstname,
+    lastname,
+    phonenumber,
+    email,
+    password
+   })
 };
 
 const register = () => {
