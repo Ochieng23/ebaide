a961c46ca0c3ea8cc9914796cd1bca21c1a875ec
paullaster <paullasterokoth98@gmail.com>
2022-12-30 01:02:44 +0300

    adding changes to a file

--- dist/index.js
+++ dist/index.js
@@ -13,20 +13,54 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module '@babel/core'\\nRequire stack:\\n- C:\\\\Users\\\\P. Haurweengs\\\\Projects\\\\content-web-app\\\\ebaide\\\\node_modules\\\\babel-loader\\\\lib\\\\index.js\\n- C:\\\\Users\\\\P. Haurweengs\\\\Projects\\\\content-web-app\\\\ebaide\\\\node_modules\\\\loader-runner\\\\lib\\\\loadLoader.js\\n- C:\\\\Users\\\\P. Haurweengs\\\\Projects\\\\content-web-app\\\\ebaide\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js\\n- C:\\\\Users\\\\P. Haurweengs\\\\Projects\\\\content-web-app\\\\ebaide\\\\node_modules\\\\webpack\\\\lib\\\\NormalModuleFactory.js\\n- C:\\\\Users\\\\P. Haurweengs\\\\Projects\\\\content-web-app\\\\ebaide\\\\node_modules\\\\webpack\\\\lib\\\\Compiler.js\\n- C:\\\\Users\\\\P. Haurweengs\\\\Projects\\\\content-web-app\\\\ebaide\\\\node_modules\\\\webpack\\\\lib\\\\webpack.js\\n- C:\\\\Users\\\\P. Haurweengs\\\\Projects\\\\content-web-app\\\\ebaide\\\\node_modules\\\\webpack\\\\lib\\\\index.js\\n- C:\\\\Users\\\\P. Haurweengs\\\\Projects\\\\content-web-app\\\\ebaide\\\\node_modules\\\\webpack-cli\\\\lib\\\\webpack-cli.js\\n- C:\\\\Users\\\\P. Haurweengs\\\\Projects\\\\content-web-app\\\\ebaide\\\\node_modules\\\\webpack-cli\\\\lib\\\\bootstrap.js\\n- C:\\\\Users\\\\P. Haurweengs\\\\Projects\\\\content-web-app\\\\ebaide\\\\node_modules\\\\webpack-cli\\\\bin\\\\cli.js\\n- C:\\\\Users\\\\P. Haurweengs\\\\Projects\\\\content-web-app\\\\ebaide\\\\node_modules\\\\webpack\\\\bin\\\\webpack.js\\n babel-loader@9 requires Babel 7.12+ (the package '@babel/core'). If you'd like to use Babel 6.x ('babel-core'), you should install 'babel-loader@7'.\\n    at Module._resolveFilename (node:internal/modules/cjs/loader:995:15)\\n    at Module._load (node:internal/modules/cjs/loader:841:27)\\n    at Module.require (node:internal/modules/cjs/loader:1061:19)\\n    at require (node:internal/modules/cjs/helpers:103:18)\\n    at Object.<anonymous> (C:\\\\Users\\\\P. Haurweengs\\\\Projects\\\\content-web-app\\\\ebaide\\\\node_modules\\\\babel-loader\\\\lib\\\\index.js:5:11)\\n    at Module._compile (node:internal/modules/cjs/loader:1159:14)\\n    at Module._extensions..js (node:internal/modules/cjs/loader:1213:10)\\n    at Module.load (node:internal/modules/cjs/loader:1037:32)\\n    at Module._load (node:internal/modules/cjs/loader:878:12)\\n    at Module.require (node:internal/modules/cjs/loader:1061:19)\");\n\n//# sourceURL=webpack://ebaide/./src/index.js?");
+eval("const register = __webpack_require__(/*! ./register */ \"./src/register.js\");\nconst name = \"ocheing\";\n\n//# sourceURL=webpack://ebaide/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/register.js":
+/*!*************************!*\
+  !*** ./src/register.js ***!
+  \*************************/
+/***/ ((module) => {
+
+eval("const newElement = tag => {\n  const newElement = document.createElement(tag);\n  return newElement;\n};\n//Get Element\nconst ElementById = id => {\n  const element = document.getElementById(id);\n  return element;\n};\n\n//Closing form on submission\nconst closeRegistrationForm = () => {\n  ElementById(\"reg_form\").style.display = \"none\";\n};\n//Error handling\nconst Error = msg => {\n  const para = newElement(\"p\");\n  para.innerHTML = msg;\n  ElementById(\"reg_form\").appendChild(para);\n};\n\n/**\r\n * @TODO clear and replace error messages on every submit with error:\r\n * @param {*} e \r\n * @returns \r\n */\nconst getData = e => {\n  e.preventDefault();\n  const firstname = ElementById(\"firstname\").value;\n  const lastname = ElementById(\"lastname\").value;\n  const phonenumber = ElementById(\"phonenumber\").value;\n  const email = ElementById(\"email\").value;\n  const password = ElementById(\"password\").value;\n  if (firstname === \"\" && lastname === \"\" && phonenumber === \"\" && email === \"\" && password === \"\") {\n    Error(\"All fields must be filled!\");\n    return;\n  }\n  if (password.length < 8) {\n    Error(\"Password must be at least 8 characters\");\n    return;\n  }\n  const body = JSON.stringify({\n    firstname,\n    lastname,\n    phonenumber,\n    email,\n    password\n  });\n  fetch(\"https://service.goebaide.com/api/auth/register\", {\n    method: \"POST\",\n    body: body,\n    headers: {\n      \"Content-Type\": \"application/json\"\n    }\n  }).then(response => {\n    return response.json();\n  }).then(data => {\n    //closeRegistrationForm();\n    console.log(data.status);\n    if (data.status === \"error\") {\n      const para = newElement(\"p\");\n      para.innerHTML = data.error;\n      console.log(para);\n      document.body.appendChild(para);\n      return;\n    }\n    ;\n    const para = newElement(\"p\");\n    para.innerHTML = data.message;\n    document.body.appendChild(para);\n  }).catch(error => {\n    const para = newElement(\"p\");\n    para.innerHTML = error.error;\n    console.log(para.innerHTML);\n    document.body.appendChild(para);\n  });\n};\nElementById(\"reg_form\").addEventListener(\"submit\", getData);\nmodule.exports = {\n  newElement,\n  ElementById\n};\n\n//# sourceURL=webpack://ebaide/./src/register.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
