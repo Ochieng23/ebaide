08a9b015034c53d86f8688d93725831fadb6f5d4
paullaster <paullasterokoth98@gmail.com>
2022-12-30 03:51:24 +0300

    adding changes to a file

--- src/login.js
+++ src/login.js
@@ -22,4 +22,114 @@ const newElement = tag => {
     para.innerHTML = msg;
     return ElementById(parentElement).appendChild(para);
   };
+
+  const getData = (e) => {
+    e.preventDefault();
+    const firstname = ElementById("firstname").value;
+    const lastname = ElementById("lastname").value;
+    const phonenumber = ElementById("phonenumber").value;
+    const email = ElementById("email").value;
+    const password = ElementById("password").value;
+    const repeatPassword = ElementById("repeat-password").value;
+    const terms = ElementById("terms_of_service");
   
+    if (ElementById("reg_form").childElementCount > 8) {
+      clearingElement (ElementById("reg_form").lastElementChild);
+      return;
+    };
+    if (
+      firstname === "" ||
+      lastname === "" ||
+      phonenumber === "" ||
+      email === "" ||
+      password === "" ||
+      repeatPassword === ""
+    ) {
+      Error("All fields must be filled!");
+      return;
+    };
+  
+    if (ElementById("reg_form").childElementCount > 8) {
+      clearingElement (ElementById("reg_form").lastElementChild);
+      return;
+    };
+  
+    if (password.length < 8) {
+      Error("Password must be at least 8 characters");
+      return;
+    };
+  
+     if (ElementById("reg_form").childElementCount > 8) {
+      clearingElement (ElementById("reg_form").lastElementChild);
+      return;
+    };
+  
+    if ( password !== repeatPassword) {
+      Error ( "Repeat password and password must be similar!" );
+      return;
+    }
+    
+    if (ElementById("reg_form").childElementCount > 8) {
+      clearingElement (ElementById("reg_form").lastElementChild);
+      return;
+    };
+  
+    if ( !terms_of_service.checked){
+      Error("Please read the Terms of Service");
+      return;
+    };
+  
+    if (ElementById("reg_form").childElementCount > 8) {
+      clearingElement (ElementById("reg_form").lastElementChild);
+      return;
+    };
+  
+    const body = JSON.stringify({
+      firstname,
+      lastname,
+      phonenumber,
+      email,
+      password
+    });
+  
+    fetch("https://service.goebaide.com/api/auth/register", {
+      method: "POST",
+      body: body,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        //closeRegistrationForm();
+        if (data.status === "error") {
+          const errorMessage = data.error.split(" ")[2];
+          const isemail= errorMessage.split("").includes ('@');
+          
+          if (isemail) {
+            Error (errorMessage + " already registered");
+            return;
+          }
+          Error (data.error);
+          return;
+        };
+  
+        if (ElementById("reg_form").childElementCount > 8) {
+          clearingElement (ElementById("reg_form").lastElementChild);
+          return;
+        };
+        successMessage ('reg_form', data.message)
+        setTimeout(window.location.href = '/index.html', 6000);
+      })
+      .catch(error => {
+        Error(error.error);
+        if (ElementById("reg_form").childElementCount > 8) {
+          clearingElement (ElementById("reg_form").lastElementChild);
+          return;
+        };
+      });
+  };
+  
+  ElementById('reg_form').addEventListener('submit', getData);
