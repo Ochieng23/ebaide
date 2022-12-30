ee54a90356244565b6264b72906318a580ae18a6
paullaster <paullasterokoth98@gmail.com>
2022-12-31 01:14:57 +0300

    commiting changes

--- src/upload.product.js
+++ src/upload.product.js
@@ -1,58 +1,63 @@
-const GetElementById  = (id) => {
-    return document.getElementById(id);
+const GetElementById = id => {
+  return document.getElementById(id);
 };
 
 const GetElementValue = (elem, id) => {
-    return elem(id).value;
+  return elem(id).value;
 };
-(GetElementValue(GetElementById, 'title-input') === '' ||
-GetElementValue(GetElementById, 'description-input')==='' ||
-GetElementValue(GetElementById, 'price-input') === '' ||
-GetElementValue(GetElementById, 'color-input') === '' ||
-GetElementValue(GetElementById, 'size-input')  === '' ||
-GetElementValue(GetElementById, 'quantity-input') === '' || )
-console.log('GetElementValue'):
-return;
-
-const productObject = JSON.stringify( {
-    title: GetElementValue(GetElementById, 'title-input'),
-    description: GetElementValue(GetElementById, 'description-input'),
-    price: GetElementValue(GetElementById, 'price-input'),
-    variation:{
-        color: GetElementValue(GetElementById, 'color-input'),
-        size: GetElementValue(GetElementById, 'size-input'),
-        quantity: GetElementValue(GetElementById, 'quantity-input'),
-    },
-    'product-image': GetElementValue(GetElementById, 'image-input'),
+if (
+  GetElementValue(GetElementById, "title-input") === "" ||
+  GetElementValue(GetElementById, "description-input") === "" ||
+  GetElementValue(GetElementById, "price-input") === "" ||
+  GetElementValue(GetElementById, "color-input") === "" ||
+  GetElementValue(GetElementById, "size-input") === "" ||
+  GetElementValue(GetElementById, "quantity-input") === "" ||
+  GetElementValue(GetElementById, "image-input") === ""
+) {
+  console.log("GetElementValue");
+  return;
+}
+const productObject = JSON.stringify({
+  title: GetElementValue(GetElementById, "title-input"),
+  description: GetElementValue(GetElementById, "description-input"),
+  price: GetElementValue(GetElementById, "price-input"),
+  variation: {
+    color: GetElementValue(GetElementById, "color-input"),
+    size: GetElementValue(GetElementById, "size-input"),
+    quantity: GetElementValue(GetElementById, "quantity-input")
+  },
+  "product-image": GetElementValue(GetElementById, "image-input")
 });
 
 const GetToken = () => {
-    const token = sessionStorage.getItem('login_token');
-    return token;
+  const token = sessionStorage.getItem("login_token");
+  return token;
 };
 
-const uploadApiUri = 'https://service.goebaide.com/api/product/new';
+const uploadApiUri = "https://service.goebaide.com/api/product/new";
 
 const PingFetchRequest = (event, uri, prod, token) => {
-    event.preventDefault();
-    fetch (uri, {
-        method: 'POST',
-        body: prod,
-        headers: {
-            'Authorization': 'Bearer '+token(),
-            'Content-Type': 'application/json',
-        },
+  event.preventDefault();
+  fetch(uri, {
+    method: "POST",
+    body: prod,
+    headers: {
+      Authorization: "Bearer " + token(),
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => {
+      return response.json();
     })
-    .then ( (response) => {
-        return response.json();
+    .then(data => {
+      console.log(data);
     })
-    .then ( (data) => {
-        console.log(data);
-    })
-    .catch ( (err) => {
-        console.log(err);
-    } );
+    .catch(err => {
+      console.log(err);
+    });
 };
 
-GetElementById ('upload_product').addEventListener ( 'submit', 
-PingFetchRequest.bind(null, uploadApiUri, productObject, GetToken));
+GetElementById("upload_product").addEventListener(
+  "submit",
+  PingFetchRequest.bind(null, uploadApiUri, productObject, GetToken)
+);
