f23b9e1bc05418b8a2ed26ee62cabf2325f59ee1
paullaster <paullasterokoth98@gmail.com>
2022-12-31 02:19:29 +0300

    commiting changes

--- src/upload.product.js
+++ src/upload.product.js
@@ -6,49 +6,50 @@ const GetElementValue = (elem, id) => {
   return elem(id).value;
 };
 
-const productObject = JSON.stringify({
-  title: GetElementValue(GetElementById, "title-input"),
-  description: GetElementValue(GetElementById, "description-input"),
-  price: GetElementValue(GetElementById, "price-input"),
-  variation: {
-    color: GetElementValue(GetElementById, "color-input"),
-    size: GetElementValue(GetElementById, "size-input"),
-    quantity: GetElementValue(GetElementById, "quantity-input")
-  },
-  "product-image": GetElementValue(GetElementById, "image-input")
-});
+// const productObject = JSON.stringify({
+//   title: GetElementValue(GetElementById, "title-input"),
+//   description: GetElementValue(GetElementById, "description-input"),
+//   price: GetElementValue(GetElementById, "price-input"),
+//   variation: {
+//     color: GetElementValue(GetElementById, "color-input"),
+//     size: GetElementValue(GetElementById, "size-input"),
+//     quantity: GetElementValue(GetElementById, "quantity-input")
+//   },
+//   "product-image": GetElementValue(GetElementById, "image-input")
+// });
 
-const GetToken = () => {
-  const token = sessionStorage.getItem("login_token");
-  return token;
-};
+// const GetToken = () => {
+//   const token = sessionStorage.getItem("login_token");
+//   return token;
+// };
 
-const uploadApiUri = "https://service.goebaide.com/api/product/new";
+// const uploadApiUri = "https://service.goebaide.com/api/product/new";
 
-const PingFetchRequest = (e) => {
-  e.preventDefault();
-  fetch(uploadApiUri, {
-    method: "POST",
-    body: productObject,
-    headers: {
-      "Authorization": "Bearer "+GetToken(),
-      "Content-Type": "application/json",
-    },
-  })
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      if ( data.error === 'jwt expired'){
-        window.location.href = '/index.html#login-form-container';
-        return;
-      };
-      console.log(data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-};
+// const PingFetchRequest = (e) => {
+//   console.log(productObject)
+//   e.preventDefault();
+//   fetch(uploadApiUri, {
+//     method: "POST",
+//     body: productObject,
+//     headers: {
+//       "Authorization": "Bearer "+GetToken(),
+//       "Content-Type": "application/json",
+//     },
+//   })
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(data => {
+//       if ( data.error === 'jwt expired'){
+//         window.location.href = '/index.html#login-form-container';
+//         return;
+//       };
+//       console.log(data);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+// };
 
 GetElementById("upload_product").addEventListener("submit", 
 PingFetchRequest);
