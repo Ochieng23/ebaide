dda43a6b670295118eb3997bb7432636fe5adf97
paullaster <paullasterokoth98@gmail.com>
2022-12-31 17:36:58 +0300

    .

--- src/upload.product.js
+++ src/upload.product.js
@@ -1,12 +1,11 @@
 const GetElementById = id => {
-  const element = document.getElementById(id)
+  const element = document.getElementById(id);
   return element;
 };
 
 const GetSelectedElementValue = (element, id) => {
   return element(id).value;
 };
-
 
 // const GetToken = () => {
 //   const token = sessionStorage.getItem("login_token");
@@ -18,37 +17,36 @@ const GetSelectedElementValue = (element, id) => {
 const PingFetchRequest = (e) => {
   e.preventDefault();
   const productObject = JSON.stringify({
-  
-  
-  "product-image": GetElementValue(GetElementById, "image-input")
-});
-
-  fetch(uploadApiUri, {
-    method: "POST",
-    body: productObject,
-    headers: {
-      "Authorization": "Bearer "+GetToken(),
-      "Content-Type": "application/json",
-    },
-  })
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      if ( data.error === 'jwt expired'){
-        window.location.href = '/index.html#login-form-container';
-        return;
-      };
-      console.log(data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    title: GetSelectedElementValue(GetElementById, "title-input"),
+    description: GetSelectedElementValue(GetElementById, "description-input"),
+    price: GetSelectedElementValue(GetElementById, "price-input"),
+    color: GetSelectedElementValue(GetElementById, "color-input"),
+    size: GetSelectedElementValue(GetElementById, "size-input"),
+    quantity: GetSelectedElementValue(GetElementById, "quantity-input"),
+    "product-image": GetElementValue(GetElementById, "image-input")
+  });
+  console.log (productObject)
+  // fetch(uploadApiUri, {
+  //   method: "POST",
+  //   body: productObject,
+  //   headers: {
+  //     Authorization: "Bearer " + GetToken(),
+  //     "Content-Type": "application/json"
+  //   }
+  // })
+  //   .then(response => {
+  //     return response.json();
+  //   })
+  //   .then(data => {
+  //     if (data.error === "jwt expired") {
+  //       window.location.href = "/index.html#login-form-container";
+  //       return;
+  //     }
+  //     console.log(data);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
 };
 
-GetElementById("upload_product").addEventListener("submit", 
-(e) => {
-    e.preventDefault();
-   // GetElementById("title-input")
-   console.log(title);
-});
+GetElementById("upload_product").addEventListener("submit", PingFetchRequest);
