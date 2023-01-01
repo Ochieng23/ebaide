e98d3dbbd8f0fb44d15adb35cd831c792a27fd6d
paullaster <paullasterokoth98@gmail.com>
2023-01-01 15:37:03 +0300

    append updates on a file

--- src/fetch.blog.js
+++ src/fetch.blog.js
@@ -65,73 +65,73 @@ const SelectElementById = id => {
           console.log(data);
           return;
         }
-        data.data.map(
-          ({
-            color,
-            description,
-            id,
-            image,
-            price,
-            quantity,
-            size,
-            title,
-            variation_id
-          }) => {
-            const divProductBox = CreateNewElement("div");
-            const productImage = CreateNewElement("img");
-            const productContent = CreateNewElement("div");
-            const titleText = CreateNewElement("h3");
-            const titleIcons = CreateNewElement("i");
-            const productDescription = CreateNewElement("p");
-            const productRating = CreateNewElement("div");
-            const productPrice = CreateNewElement("div");
-            const productPriceEstimate = CreateNewElement("span");
-            const buyNowBtn = CreateNewElement("a");
+        // data.data.map(
+        //   ({
+        //     color,
+        //     content,
+        //     id,
+        //     image,
+        //     price,
+        //     quantity,
+        //     size,
+        //     title,
+        //     variation_id
+        //   }) => {
+        //     const divProductBox = CreateNewElement("div");
+        //     const productImage = CreateNewElement("img");
+        //     const productContent = CreateNewElement("div");
+        //     const titleText = CreateNewElement("h3");
+        //     const titleIcons = CreateNewElement("i");
+        //     const productDescription = CreateNewElement("p");
+        //     const productRating = CreateNewElement("div");
+        //     const productPrice = CreateNewElement("div");
+        //     const productPriceEstimate = CreateNewElement("span");
+        //     const buyNowBtn = CreateNewElement("a");
   
-            AddClassToNewElement(divProductBox, "box");
-            AddAttributeToElement(divProductBox, "id", id);
+        //     AddClassToNewElement(divProductBox, "box");
+        //     AddAttributeToElement(divProductBox, "id", id);
   
-            AddAttributeToElement(productImage, "src", 
-            `https://service.goebaide.com/${image}`);
-            AddAttributeToElement(productImage, "alt", "product image");
-            AppendChildToParentElementUsingElement(divProductBox, productImage)
+        //     AddAttributeToElement(productImage, "src", 
+        //     `https://service.goebaide.com/${image}`);
+        //     AddAttributeToElement(productImage, "alt", "product image");
+        //     AppendChildToParentElementUsingElement(divProductBox, productImage)
   
-            AddClassToNewElement(productContent, "content");
+        //     AddClassToNewElement(productContent, "content");
   
-            AddClassToNewElement (titleIcons, "fas");
-            AddClassToNewElement (titleIcons, "fa-map-marker-alt");
-            titleText.innerHTML = title;
-            AppendElementAsFirstChildUsingElement(titleText, titleIcons);
-            AppendChildToParentElementUsingElement(productContent, titleText);
+        //     AddClassToNewElement (titleIcons, "fas");
+        //     AddClassToNewElement (titleIcons, "fa-map-marker-alt");
+        //     titleText.innerHTML = title;
+        //     AppendElementAsFirstChildUsingElement(titleText, titleIcons);
+        //     AppendChildToParentElementUsingElement(productContent, titleText);
   
-            productDescription.innerHTML = `<h2>Product Description</h2> \n` +description;
-            AppendChildToParentElementUsingElement (productContent, productDescription);
+        //     productDescription.innerHTML = `<h2>Product Description</h2> \n` +description;
+        //     AppendChildToParentElementUsingElement (productContent, productDescription);
             
-           AddClassToNewElement (productRating, "stars");
-           for (let i= 0; i < 5; i++) {
-              const ratingStars = CreateNewElement("i");
-              AddClassToNewElement (ratingStars, "fas");
-              AddClassToNewElement (ratingStars, "fa-star");
-              AppendChildToParentElementUsingElement (productRating, ratingStars);
-           };
-           AppendChildToParentElementUsingElement (productContent, productRating);
+        //    AddClassToNewElement (productRating, "stars");
+        //    for (let i= 0; i < 5; i++) {
+        //       const ratingStars = CreateNewElement("i");
+        //       AddClassToNewElement (ratingStars, "fas");
+        //       AddClassToNewElement (ratingStars, "fa-star");
+        //       AppendChildToParentElementUsingElement (productRating, ratingStars);
+        //    };
+        //    AppendChildToParentElementUsingElement (productContent, productRating);
   
-           AddClassToNewElement (productPrice, 'price');
-           productPrice.innerHTML = "Ksh. "+price;
-           productPriceEstimate.innerHTML = "Ksh. "+(price + 40);
-           AppendChildToParentElementUsingElement(productPrice, productPriceEstimate);
-           AppendChildToParentElementUsingElement(productContent, productPrice);
+        //    AddClassToNewElement (productPrice, 'price');
+        //    productPrice.innerHTML = "Ksh. "+price;
+        //    productPriceEstimate.innerHTML = "Ksh. "+(price + 40);
+        //    AppendChildToParentElementUsingElement(productPrice, productPriceEstimate);
+        //    AppendChildToParentElementUsingElement(productContent, productPrice);
   
-           AddClassToNewElement (buyNowBtn, 'btn');
-           AddAttributeToElement(buyNowBtn, "href", "#");
-           AppendChildToParentElementUsingElement(productContent, buyNowBtn);
+        //    AddClassToNewElement (buyNowBtn, 'btn');
+        //    AddAttributeToElement(buyNowBtn, "href", "#");
+        //    AppendChildToParentElementUsingElement(productContent, buyNowBtn);
   
-           AppendChildToParentElementUsingElement(divProductBox, productContent);
+        //    AppendChildToParentElementUsingElement(divProductBox, productContent);
   
-           AppendChildToParentElement("box-container", 
-           GetSelectedElementWithClassName, divProductBox);
-          }
-        );
+        //    AppendChildToParentElement("box-container", 
+        //    GetSelectedElementWithClassName, divProductBox);
+        //   }
+        // );
       })
       .catch(err => {
         console.error(err);
