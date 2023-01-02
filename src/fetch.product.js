f5034eb07a2d76673a25165726e8a988eb88a229
paullaster <paullasterokoth98@gmail.com>
2023-01-02 22:07:43 +0300

    add file changes

--- src/fetch.product.js
+++ src/fetch.product.js
@@ -172,7 +172,7 @@ const PullProductsFromBackendAPI = () => {
                     "view-single-product"
                   );
                   alert(productContainer);
-                }, 5000);
+                }, 2000);
               }
             })
             .catch(error => {
