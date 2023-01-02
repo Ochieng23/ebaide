988e044f0eab0b10c6f506f6852e55ab7529fed7
paullaster <paullasterokoth98@gmail.com>
2023-01-02 23:55:38 +0300

    add file changes

--- src/fetch.product.js
+++ src/fetch.product.js
@@ -270,8 +270,8 @@ const PullProductsFromBackendAPI = () => {
                       productTitle
                     )
 
+                    const productPriceSpanHolder = CreateNewElement('h2');
 
-                    
                     AppendChildToParentElementUsingElement(
                       productBox, 
                       productInformation
