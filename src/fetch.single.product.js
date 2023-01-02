99edefd6860de1c56652c8b3dff1d0a10e7405cd
paullaster <paullasterokoth98@gmail.com>
2023-01-02 19:30:59 +0300

    add file changes

--- src/fetch.single.product.js
+++ src/fetch.single.product.js
@@ -1,9 +1,1 @@
-const FindItemsByClassName = (className) => {
-    return document.querySelectorAll(className);
-};
-
-const productCollection = 
-          GetSelectedElementsWithClassName('.view-product');
-          for (let i = 0; i < productCollection.length; i++) {
-            console.log(productCollection.item(i));
-          };
+document.addEventListener ('load', () => {})
