b874cc7ad20390cead808b794be423404fb8fd24
paullaster <paullasterokoth98@gmail.com>
2023-01-03 00:33:24 +0300

    add file changes

--- src/fetch.product.js
+++ src/fetch.product.js
@@ -337,6 +337,10 @@ const PullProductsFromBackendAPI = () => {
                     const addToCartButton = CreateNewElement ('button');
                     AddClassToNewElement(addToCartButton, 'btn');
                     addToCartButton.innerHTML = 'Add To Cart';
+                    AppendChildToParentElementUsingElement(
+                      productInformation,
+                      addToCartButton
+                    );
 
                     AppendChildToParentElementUsingElement(
                       productBox, 
