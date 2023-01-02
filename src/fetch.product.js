42e90885f88a48577c69d435dc8b708fc63d0f36
paullaster <paullasterokoth98@gmail.com>
2023-01-02 23:43:06 +0300

    add file changes

--- src/fetch.product.js
+++ src/fetch.product.js
@@ -251,6 +251,7 @@ const PullProductsFromBackendAPI = () => {
                   AddClassToNewElement(productInformation, 'col-12');
                   AddClassToNewElement(productInformation, 'my-5');
                   AddClassToNewElement(productInformation, 'pt-5');
+                    
 
                   AppendChildToParentElementUsingElement(
                     productContainer,
