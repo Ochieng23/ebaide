ac2549ed6f3aae3b1302215a8d00d67ad7ed9fc1
paullaster <paullasterokoth98@gmail.com>
2023-01-02 17:25:53 +0300

    add file changes

--- src/fetch.single.product.js
+++ src/fetch.single.product.js
@@ -1,6 +1,7 @@
 const FindItemsByClassName = (name) => {
     const elements = document.querySelectorAll (name);
     return elements;
-}
+};
 console.log(FindItemsByClassName ('.box'));
 
+const GetingClickedItemInAList =  (cb, className) => {};
