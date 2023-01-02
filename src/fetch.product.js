f5797fb4d7a36976134a0def7a079daed0403d2b
paullaster <paullasterokoth98@gmail.com>
2023-01-02 22:33:29 +0300

    add file changes

--- src/fetch.product.js
+++ src/fetch.product.js
@@ -180,6 +180,8 @@ const PullProductsFromBackendAPI = () => {
                   const productFirstImage = CreateNewElement ('img');
                   AddClassToNewElement (productFirstImage, "mt-5");
                   AddClassToNewElement (productFirstImage, "img-fluid");
+                  AddClassToNewElement (productFirstImage, "w-100");
+                  AddAttributeToElement(productFirstImage, "alt", "product first image");
 
 
                   AppendChildToParentElementUsingElement (productContainer, productBox);
