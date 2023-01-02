8a39d36d56cd1b4cc85c0d271b7f5583146b587f
paullaster <paullasterokoth98@gmail.com>
2023-01-02 18:49:37 +0300

    add file changes

--- src/fetch.single.product.js
+++ src/fetch.single.product.js
@@ -1,5 +1,6 @@
 const FindParentProductHolder = (id) => {
     return document.getElementById (id);
 };
-const test = FindParentProductHolder ('view-single-product');
+
+FindParentProductHolder ('view-single-product');
 console.log (test.children);
