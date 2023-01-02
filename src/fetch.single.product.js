603f93303f05f6c2e88ecebfbfd51bb7a06a8bf2
paullaster <paullasterokoth98@gmail.com>
2023-01-02 17:45:56 +0300

    add file changes

--- src/fetch.single.product.js
+++ src/fetch.single.product.js
@@ -2,15 +2,15 @@ const FindItemsByClassName = (name) => {
     const elements = document.querySelectorAll (name);
     return elements;
 };
+console.log(FindItemsByClassName ('.view-product'))
+// const DisplaysingleProduct = () => {
+//     console.log('DisplaysingleProduct');
+// };
 
-const DisplaysingleProduct = () => {
-    console.log('DisplaysingleProduct');
-};
-
-const GetingClickedItemInAList =  (cb1, cb2, className) => {
-    for (let i = 0; i < cb1(className).length; i++) {
-        cb1(className)[i].addEventListener ('click', cb2);
-    };
-};
+// const GetingClickedItemInAList =  (cb1, cb2, className) => {
+//     for (let i = 0; i < cb1(className).length; i++) {
+//         cb1(className)[i].addEventListener ('click', cb2);
+//     };
+// };
 
 GetingClickedItemInAList (FindItemsByClassName, DisplaysingleProduct, '.view-product')
