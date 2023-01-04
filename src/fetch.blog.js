241b2f985b2d90b26217216262b3a3ecc06e02ec
paullaster <paullasterokoth98@gmail.com>
2023-01-05 02:09:46 +0300

    appending file changes

--- src/fetch.blog.js
+++ src/fetch.blog.js
@@ -105,7 +105,7 @@ const PostedDateUpdater = (cb, d) => {
           continue;
         }else{
           let textString = temp[prop] > 1 ? "days ago" :'day ago';
-          outputSincePosted += temp[prop] + textString;
+          outputSincePosted = [...outputSincePosted, temp[prop] + textString];
         };
         break;
       case 'hours':
