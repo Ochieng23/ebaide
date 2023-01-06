4e28951586736eac0afd0daa3626945ddcb4e773
paullaster <paullasterokoth98@gmail.com>
2023-01-06 20:00:18 +0300

    add file changes

--- src/fetch.product.js
+++ src/fetch.product.js
@@ -352,6 +352,7 @@ const PullProductsFromBackendAPI = () => {
               const {id, title, image} = pickedItem[0];
               const itemSize = sizeList.value;
               const itemQuantityToBuy = quantityToBuy.value;
+              const cartArrayItem = {}
               //Handling Clicking Go To Cart Button
             });
 
