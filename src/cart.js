a9f2416078eabf5cc7b951aff4f00ba0343cda3c
paullaster <paullasterokoth98@gmail.com>
2023-01-06 07:15:02 +0300

    add changes

--- src/cart.js
+++ src/cart.js
@@ -1,28 +1,39 @@
-const FindSingleElement = (identifer) => {
-    const element = document.querySelector(identifer);
-    return element;
+const FindSingleElement = identifer => {
+  const element = document.querySelector(identifer);
+  return element;
 };
 
-const FindSingleElementByID = (identifer) => {
-    const element = document.getElementById(identifer);
-    return element;
+// const FindSingleElementByID = (identifer) => {
+//     const element = document.getElementById(identifer);
+//     return element;
+// };
+
+const ClearContent = (cb, identifer) => {
+  const element = cb(identifer);
+  element.innerHTML = "";
 };
 
-const ClearContent = (cd, identifer) => {
-    const element = cb (identifer);
+const CreateNewHTMLElement = tag => {
+  const newElement = document.createElement(tag);
+  return newElement;
 };
 
-const CreateNewHTMLElement = (tag) => {
-    const newElement = document.createElement(tag);
-    return newElement;
+const AppendHTMLChildToStaticElement = (indentifier, cb, child) => {
+  cb(indentifier).appendChild(child);
 };
 
-const AppendHTMLChild = (indetifier, cb)
+const Cart = e => {
+  e.preventDefault();
+  ClearContent(FindSingleElement, "#body");
+  setTimeout(() => {
+    const emptyCartTextSpan = CreateNewHTMLElement("span");
+    emptyCartTextSpan.innerHTML = "Your Cart is empty";
+    AppendHTMLChildToStaticElement(
+      "#body",
+      FindSingleElement,
+      emptyCartTextSpan
+    );
+  }, 1500);
+};
 
-const Cart = (e) => {
-    e.preventDefault();
-}
-
-FindSingleElement ('.cart').addEventListener('click', () => {
-
-})
+FindSingleElement(".cart").addEventListener("click", Cart);
