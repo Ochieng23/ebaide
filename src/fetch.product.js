2e139292da4c84806a98f87e0f7d9310ac3756a9
paullaster <paullasterokoth98@gmail.com>
2023-01-06 20:29:04 +0300

    add file changes

--- src/fetch.product.js
+++ src/fetch.product.js
@@ -361,7 +361,8 @@ const PullProductsFromBackendAPI = () => {
               };
 
               const storedCartItems = JSON.parse(localStorage.getItem('cart-items'));
-              if(storedCartItems.length > 0){};
+              
+              
               cartArray = [cartArrayItem];
               localStorage.setItem('cart-items', JSON.stringify(cartArray));
               //Handling Clicking Go To Cart Button
