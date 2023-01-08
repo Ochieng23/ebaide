03c6d63ac1e7cce6685b73a57bda87e471e30ba0
paullaster <paullasterokoth98@gmail.com>
2023-01-08 17:26:37 +0300

    commit file changes

--- src/fetch.product.js
+++ src/fetch.product.js
@@ -348,7 +348,7 @@ const PullProductsFromBackendAPI = () => {
 
             addToCartButton.addEventListener("click", e => {
               e.preventDefault();
-              console.log(e)
+              //console.log(e)
               RemoveClassFromNewElement(addToCartButtonIcon, "fa-cart-plus");
               AddClassToNewElement(addToCartButtonIcon, "fa-shopping-cart");
               AddClassToNewElement(addToCartButton, "view-cart-button");
