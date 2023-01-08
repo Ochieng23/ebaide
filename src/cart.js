b4524b8a9e1587765b8c8b2fd81e7af04243a614
paullaster <paullasterokoth98@gmail.com>
2023-01-08 22:25:04 +0300

    commit file changes

--- src/cart.js
+++ src/cart.js
@@ -30,7 +30,7 @@ const AddClass = (element, className) => {
   element.classList.add(className);
 };
 
-const CartTableHeadingsArray = ['image', 'title', 'size', 'quantity', 'price per item'];
+const CartTableHeadingsArray = ['image', 'title', 'size', 'quantity', 'price per item', 'remove'];
 
 const DisplayCartItems = (arrOfCartItems) => {
   const displayTable = CreateNewHTMLElement (table);
