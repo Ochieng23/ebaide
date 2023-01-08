c1828f83e302aea6e1bd9eb2cb054e34703262e2
paullaster <paullasterokoth98@gmail.com>
2023-01-08 23:14:51 +0300

    commit file changes

--- src/cart.js
+++ src/cart.js
@@ -56,6 +56,7 @@ const DisplayCartItems = (cartItemsArray) => {
   const displayTableBodyRow = CreateNewHTMLElement('tr');
   cartItemsArray.map ( (cartItem) =>{
     const tableImage = CreateNewHTMLElement ('img');
+    AddAttributeToElement()
     tableImage.innerHTML = `https://service.goebaide.com/${cartItem.image}`;
     AppendChildToDynamicElement (displayTableBodyRow, tableImage);
   })
