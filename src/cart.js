39b7cc0e05745280639cf55c9610aee8d7eb0127
paullaster <paullasterokoth98@gmail.com>
2023-01-08 23:35:22 +0300

    commit file changes

--- src/cart.js
+++ src/cart.js
@@ -69,6 +69,9 @@ const DisplayCartItems = cartItemsArray => {
       <td>
        ${cartItem.itemQuantityToBuy}
       </td>
+      <td>
+       ${cartItem.itemQuantityToBuy}
+      </td>
 
     </tr>
     `;
