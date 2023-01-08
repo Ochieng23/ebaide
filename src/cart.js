eda125322b4a1d8fdcc85096d1ab0e3909d274c7
paullaster <paullasterokoth98@gmail.com>
2023-01-08 22:26:39 +0300

    commit file changes

--- src/cart.js
+++ src/cart.js
@@ -42,7 +42,7 @@ const CartTableHeadingsArray = [
 const DisplayCartItems = arrOfCartItems => {
   const displayTable = CreateNewHTMLElement(table);
   const displayTableHeader = CreateNewHTMLElement(thead);
-  for (let i = 0; i < CartTableHeadingsArray.length; i++) {
+  for (let i = 0; i < CartTableHeadingsArray.length; i++) {};
 };
 
 const Cart = e => {
