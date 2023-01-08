83b48e76885ea61f7f50b00448762169bd1d2ba2
paullaster <paullasterokoth98@gmail.com>
2023-01-08 23:00:40 +0300

    commit file changes

--- src/cart.js
+++ src/cart.js
@@ -63,6 +63,7 @@ const DisplayCartItems = (cartItemsArray) => {
       AddClass (trashIcon, 'fa-trash-o');
       AppendChildToDynamicElement (tableData, trashIcon);
     }
+    AppendChildToDynamicElement (displayTableBodyRow, tableData);
   };
   
   
