149379ea22d16e2377cefcb39e76526e96a6ede6
paullaster <paullasterokoth98@gmail.com>
2023-01-09 06:38:26 +0300

    adding functionality to cart

--- src/cart.js
+++ src/cart.js
@@ -80,7 +80,7 @@ const DisplayCartItems = (cartItemsArray) => {
 
       const displayItemQuantity = CreateNewHTMLElement ('span');
       AddClass(displayItemQuantity, 'display-item-title');
-      AddClass(displayItemQuantity, 'display-item-title');
+      AddClass(displayItemQuantity, 'cart-items-fourth-last-column');
       AddClass (displayItemQuantity, 'cart-display-item-row');
       displayItemQuantity.innerHTML = cartItemsArray[k].itemQuantityToBuy; 
       AppendChildToDynamicElement(displayBodyRow, displayItemQuantity);
