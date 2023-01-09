decb9727d421b02729838e716375df85adb7fe57
paullaster <paullasterokoth98@gmail.com>
2023-01-09 21:09:47 +0300

    commiting file changes

--- src/cart.js
+++ src/cart.js
@@ -93,6 +93,7 @@ const DisplayCartItems = cartItemsArray => {
      //Delete Product:
      const deleteItem = CreateNewHTMLElement("td");
      const deleteItemIcon = CreateNewHTMLElement("i");
+     AddClass(deleteItem, 
      AppendChildToDynamicElement(itemsRow, );
 
     AppendChildToDynamicElement(tableBody, itemsRow);
