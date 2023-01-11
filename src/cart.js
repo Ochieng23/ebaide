c1336c0344f473ac7d7a8d37383f7480329192d5
paullaster <paullasterokoth98@gmail.com>
2023-01-11 15:21:49 +0300

    add changes to development code

--- src/cart.js
+++ src/cart.js
@@ -127,7 +127,7 @@ const DisplayCartItems = cartItemsArray => {
 
       const updatedList = storedItems.filter(item => item.id !== itemID);
       localStorage.setItem("cart-items", JSON.stringify(updatedList));
-      ClearContent(FindSingleElement, ".cart-container");
+      ClearContent(FindSingleElement, "#table");
       //Cart();
       DisplayCartItems(updatedList);
     });
