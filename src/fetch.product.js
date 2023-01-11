95b1f31dfaaf3162fd04b99999edb68139ba178b
paullaster <paullasterokoth98@gmail.com>
2023-01-11 17:54:52 +0300

    add files mods

--- src/fetch.product.js
+++ src/fetch.product.js
@@ -424,6 +424,7 @@ const PullProductsFromBackendAPI = () => {
                 });
                 return;
               }
+              C
               PullProductsFromBackendAPI ();
               window.location.reload();
               setTimeout(() => {
