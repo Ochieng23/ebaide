1c76ce7c619a75d16802ad6789d66d5d1c4fd117
paullaster <paullasterokoth98@gmail.com>
2023-01-11 17:28:58 +0300

    add files mods

--- src/fetch.product.js
+++ src/fetch.product.js
@@ -147,8 +147,8 @@ const PullProductsFromBackendAPI = () => {
           if (isProductInCart === undefined){
             buyNowBtn.innerHTML = "Buy Now";
           };
-          console.log (isProductInCart);
-          buyNowBtn.innerHTML = "Go to Cart";
+          
+          //buyNowBtn.innerHTML = "Go to Cart";
           AddClassToNewElement(buyNowBtn, "view-product");
           AddClassToNewElement(buyNowBtn, "btn");
           AddAttributeToElement(buyNowBtn, "type", "button");
