98962c58e3e1f0068631802426894b3be7785b41
paullaster <paullasterokoth98@gmail.com>
2023-01-15 22:49:48 +0300

    commit file changes

--- src/cart.js
+++ src/cart.js
@@ -237,7 +237,7 @@ const CheckoutPage =() => {
   let order = JSON.parse(
     sessionStorage.getItem ("order-items")
   );
-  order.map((orderItem) => {
+  let amount = order.map((orderItem) => {
     if (orderItem.amount){
       return orderItem.amount;
     }
