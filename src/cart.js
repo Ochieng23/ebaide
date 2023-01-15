5f02cdf0b958b83b081c247d80295ea648bdde44
paullaster <paullasterokoth98@gmail.com>
2023-01-15 22:47:32 +0300

    commit file changes

--- src/cart.js
+++ src/cart.js
@@ -237,7 +237,9 @@ const CheckoutPage =() => {
   let order = JSON.parse(
     sessionStorage.getItem ("order-items")
   );
-  order.map((orderItem));  
+  order.map((orderItem) => {
+    
+  });  
   discountValue.innerHTML = '';
   AppendChildToDynamicElement(orderDiscount, discountValue);
   AppendChildToDynamicElement(orderSummeryBodySection, orderDiscount);
