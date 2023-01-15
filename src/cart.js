d0545154bbfbc2c88af3fa136022b484622adbfb
paullaster <paullasterokoth98@gmail.com>
2023-01-15 22:27:42 +0300

    commit file changes

--- src/cart.js
+++ src/cart.js
@@ -232,6 +232,8 @@ const CheckoutPage =() => {
   discountText.innerHTML = 'Discount 10% OFF';
   AppendChildToDynamicElement(orderDiscount, discountText);
   const discountValue = CreateNewHTMLElement("span");
+
+
   
   discountValue.innerHTML = '';
   AppendChildToDynamicElement(orderDiscount, discountValue);
