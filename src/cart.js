e99ed26e5392df25a1a0f3cc439eb54e3a6fb46c
paullaster <paullasterokoth98@gmail.com>
2023-01-15 21:28:36 +0300

    commit file changes

--- src/cart.js
+++ src/cart.js
@@ -217,6 +217,8 @@ const Cart = () => {
 const CheckoutPage =() => {
   const checkOutDiv = CreateNewHTMLElement('div');
   const checkoutButton = CreateNewHTMLElement('button');
+  checkoutButton.innerHTML = 'Checkout';
+  
   AppendChildToDynamicElement (checkOutDiv, checkoutButton)
   AppendChildToDynamicElement (checkoutContainer, checkOutDiv);
   AppendHTMLChildToStaticElement("#body", FindSingleElement, checkoutContainer);
