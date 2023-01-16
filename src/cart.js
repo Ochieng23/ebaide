4fe5441369904d561ae956646acaecd5be5bcfe2
paullaster <paullasterokoth98@gmail.com>
2023-01-16 15:37:01 +0300

    append changes

--- src/cart.js
+++ src/cart.js
@@ -351,7 +351,7 @@ const PaymentPhoneNumber = () => {
     FindSingleElement,
     phoneNumberPageContainer
   );
-  
+  nextButton.addEventListener("click", () => {});
 };
 
 const UserAddress = () => {
