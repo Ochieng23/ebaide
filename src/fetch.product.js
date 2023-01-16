5d08c18031287e69e1503b4e029c3314404d7186
paullaster <paullasterokoth98@gmail.com>
2023-01-16 13:49:47 +0300

    append changes

--- src/fetch.product.js
+++ src/fetch.product.js
@@ -343,7 +343,7 @@ const PullProductsFromBackendAPI = () => {
             addToCartButton.addEventListener("click", e => {
               e.preventDefault();
               //console.log(e)
-              if (){};
+              if (quantityToBuy.value < 1){};
               RemoveClassFromNewElement(addToCartButtonIcon, "fa-cart-plus");
               AddClassToNewElement(addToCartButtonIcon, "fa-shopping-cart");
               AddClassToNewElement(addToCartButton, "view-cart-button");
