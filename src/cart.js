7fb7264c17f0c980c57f4eb95c7b0b38e8309f85
paullaster <paullasterokoth98@gmail.com>
2023-01-16 21:51:17 +0300

    append changes

--- src/cart.js
+++ src/cart.js
@@ -322,8 +322,10 @@ const CheckoutPage = () => {
   //SENDING ORDER TO BACKEND SERVICE:
   checkoutButton.addEventListener ("click", () => {
      const orderItems = JSON.parse(sessionStorage.getItem("order-items"));
-     
-     console.log(orderItems);
+     const orderObject = {
+       data: orderItems
+     };
+     console.log(orderObject);
      //SENDING VIA FETCH TO BACKEND
      const order_processing_URI = `http://localhost:3000/api/orders/new-order`;
     //  fetch(order_processing_URI, {
