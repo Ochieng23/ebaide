bbde2a0d9707793ad025d4db22922d6d96f54c22
paullaster <paullasterokoth98@gmail.com>
2023-01-16 15:31:17 +0300

    append changes

--- src/cart.js
+++ src/cart.js
@@ -334,11 +334,11 @@ const PaymentPhoneNumber = () => {
     "Use format e.g. 254700258098"
   );
   AddAttributeToElement(phoneNumberInput, "required", true);
-  
   AppendChildToDynamicElement(phoneNumberLabel, phoneNumberInput);
   AppendChildToDynamicElement(phoneNumberDiv, phoneNumberLabel);
 
   AppendChildToDynamicElement(form, phoneNumberDiv);
+  
   AppendChildToDynamicElement(formHolder, form);
   AppendChildToDynamicElement(phoneNumberPageContainer, formHolder);
   AppendHTMLChildToStaticElement(
