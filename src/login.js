8af090b3aaef45e9787a85206a043f9d4b7da96f
paullaster <paullasterokoth98@gmail.com>
2023-01-20 05:33:14 +0300

    commit changes

--- src/login.js
+++ src/login.js
@@ -76,7 +76,7 @@ const onSuccessMessage = (cb1, cb2, tag, parent, message) => {
         //console.log(data.token);
         sessionStorage.setItem("login_token", data.token);
         clearingContent ("login-container-div", ElementById);
-        onSuccessMessage ()
+        onSuccessMessage (newElement, )
         //setTimeout(window.location.href = '/index.html#packages', 3500);
       
       })
